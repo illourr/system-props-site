f46d365f16d9eae51882b23c2406579d16504f05
Rogin Farrer <rogin@roginfarrer.com>
2021-05-28 15:45:49 -0400

    try webpack 5

--- next.config.js
+++ next.config.js
@@ -3,40 +3,10 @@ const withVideos = require('next-videos');
 const withOptimizedImages = require('next-optimized-images');
 
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+  target: 'serverless',
+  future: {
+    webpack5: true,
   },
 };
 
 module.exports = withPlugins([withOptimizedImages, withVideos], nextConfig);
-
-/**
- *
- * @param {string} resourcePath
- *
- * Make an ID from a path
- *
- * Currently, `resourcePath` is formatted like this:
- * 	- "blog/name-of-file.mdx"
- * 	- "blog/name-of-folder/index.mdx"
- *
- * This function will make the following IDs:
- * 	- "name-of-file"
- * 	- "name-of-folder"
- *
- */
-function makeIdFromPath(resourcePath) {
-  const parts = resourcePath.split('/');
-  const articleType = parts[0];
-  let lastPart = parts[parts.length - 1];
-
-  if (lastPart.includes('index')) {
-    lastPart = parts[parts.length - 2];
-  }
-
-  return `${articleType}/${lastPart.replace('.mdx', '')}`;
-}
